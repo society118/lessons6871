03406cdc4358d8f1e18496dbcd6e449bf473a3be
society118 <glebgrigorenko35@gmail.com>
2025-05-27 21:30:17 +0300

    Classwork

---
+++ 3.py
@@ -0,0 +1,11 @@
+
+password = "society"
+
+
+user_input = input("Будь ласка, введіть пароль: ")
+
+
+if user_input == password:
+    print("Пароль вірний! Доступ дозволено.")
+else:
+    print("Невірний пароль. Спробуйте ще раз.")
